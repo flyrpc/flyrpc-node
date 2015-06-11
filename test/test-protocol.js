3d1cf513bd216ab27d9659e2c75d993a6276e3a0
Gui Lin <guileen@gmail.com>
2015-06-12 02:20:24 +0800

    update framework

--- test/test-protocol.js
+++ test/test-protocol.js
@@ -1,12 +1,20 @@
 var net = require('net')
 
 describe('protocol', function() {
+    var echoServer, socket
+    var receivedPackets = []
 
-    before(function() {
-        var socket = net.connect()
+    before(function(done) {
+        echoServer = net.createServer(function(sock) {
+            sock.on('data', function(data) {
+                sock.write(data)
+            })
+        })
+        echoServer.listen(18000)
+        var socket = net.connect(18000, done)
     })
 
-    it('', function() {
+    it('should parse packet', function() {
 
     })
 })
